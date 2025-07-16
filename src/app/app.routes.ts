5cfd5b738a68d1be7432ca185ffa2f3611a8ce51
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2025-07-16 16:18:42 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { dishesAlResolver } from './shared/resolver/dishes/dishes-al/dishes-al.resolver';
 import { lastRecipesResolver } from './shared/resolver/recipe/last-recipe/last-recipe.resolver';
-import { NgModule } from '@angular/core';
-import { PrivacyPolicyComponent } from './shared/components/privacy-policy/privacy-policy.component';
-import { SearchComponent } from './shared/components/search/search.component';
-import { DishFilterComponent } from './shared/components/dish-filter/dish-filter.component';
-import { filterResolver } from './shared/resolver/filter/filter.resolver';
 
 export const routes: Routes = [
     {
@@ -19,42 +14,4 @@ export const routes: Routes = [
 
 
     },
-
-    {
-        path: 'search',
-        component: SearchComponent,
-
-    },
-    {
-        path: 'recipe-filte/:filterType/:slug',
-        component: DishFilterComponent,
-        resolve: {
-            recipes: filterResolver
-        }
-    },
-
-
-
-
-
-
-
-    {
-        path: 'privacyy', component: PrivacyPolicyComponent
-    },
-
-    {
-        path: '**',
-        loadComponent: () => import('./../app/shared/components/soft-404/soft-404.component').then(m => m.Soft404Component)
-    }
-
 ];
-
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
-    exports: [RouterModule]
-})
-
-
-export class AppRoutingModule { }
