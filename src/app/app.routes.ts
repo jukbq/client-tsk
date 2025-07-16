28f39925e3d8a21cb5f410144db229bb34fdb378
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2025-07-16 16:12:04 +0300

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { dishesAlResolver } from './shared/resolver/dishes/dishes-al/dishes-al.resolver';
+import { lastRecipesResolver } from './shared/resolver/recipe/last-recipe/last-recipe.resolver';
+import { NgModule } from '@angular/core';
+import { PrivacyPolicyComponent } from './shared/components/privacy-policy/privacy-policy.component';
+import { SearchComponent } from './shared/components/search/search.component';
+import { DishFilterComponent } from './shared/components/dish-filter/dish-filter.component';
+import { filterResolver } from './shared/resolver/filter/filter.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/home/home.component').then((m) => m.HomeComponent),
+        resolve: {
+            dishes: dishesAlResolver,
+            recentRecipe: lastRecipesResolver,
+        },
+
+
+    },
+
+    {
+        path: 'search',
+        component: SearchComponent,
+
+    },
+    {
+        path: 'recipe-filte/:filterType/:slug',
+        component: DishFilterComponent,
+        resolve: {
+            recipes: filterResolver
+        }
+    },
+
+
+
+
+
+
+
+    {
+        path: 'privacyy', component: PrivacyPolicyComponent
+    },
+
+    {
+        path: '**',
+        loadComponent: () => import('./../app/shared/components/soft-404/soft-404.component').then(m => m.Soft404Component)
+    }
+
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+    exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,191 @@
+import { CommonModule, DOCUMENT, isPlatformBrowser, PlatformLocation, ViewportScroller } from '@angular/common';
+import { Component, ElementRef, HostListener, Inject, PLATFORM_ID, Renderer2, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { SeoService } from '../../shared/services/seo/seo.service';
+import { Meta, Title } from '@angular/platform-browser';
+import { DishesService } from '../../shared/services/dishes/dishes.service';
+import { RecipeService } from '../../shared/services/recipe/recipe.service';
+import { CategoriesService } from '../../shared/services/categories/categories.service';
+import { FooyerComponent } from "../../shared/components/fooyer/fooyer.component";
+import { SsrLinkDirective } from '../../shared/directives/ssr-link.directive';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, NgbCarouselModule, FooyerComponent, SsrLinkDirective],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  @ViewChild('textBlocks') textBlocksRef!: ElementRef<HTMLDivElement>;
+  isVisible = false;
+  recipes: any = [];
+  currentURL = '';
+  dishesList: any[] = [];
+  recipeCounts: { [key: string]: number } = {};
+  dishCategories: { [key: string]: any[] } = {};
+  isBrowser: boolean = false;
+
+
+
+  //contennt
+  homeTite = '';
+  mainImage = 'https://firebasestorage.googleapis.com/v0/b/synikit-12dee.appspot.com/o/home%2Fbackground.webp?alt=media&token=fd797c35-8b8c-4c3e-a51c-c64679a02814';
+  schema: any;
+
+  private ldJsonScript?: HTMLScriptElement;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private seoServices: SeoService,
+    private route: ActivatedRoute,
+    private meta: Meta,
+    private viewportScroller: ViewportScroller,
+    private titleService: Title,
+    private renderer: Renderer2,
+    private router: Router,
+    private dishesService: DishesService,
+    private categoryService: CategoriesService,
+    private recipeService: RecipeService
+  ) { this.isBrowser = isPlatformBrowser(this.platformId); }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    }
+    this.route.data.subscribe((data: any) => {
+      const wrapper = data.dishes
+      const dishes = wrapper.data;
+      this.currentURL = wrapper.url;
+      this.loadData();
+      this.dishesList = dishes
+      this.dishesList.sort((a, b) => a.dishesName.localeCompare(b.dishesName));
+      this.recipeCountLoad()
+      this.recipes = data.recentRecipe
+
+    });
+  }
+
+  loadData() {
+    this.homeTite = 'Таверна "Синій Кіт" – перевірені рецепти та поради'
+    this.seoServices.setCanonicalUrl(this.currentURL)
+    this.titleService.setTitle(this.homeTite);
+    this.meta.updateTag({ property: 'canonical', content: this.currentURL });
+    this.meta.updateTag({
+      name: 'description',
+      content: `Таверна "Синій Кіт" — це прості рецепти, смачні ідеї, соковите м'ясо, овочі, випічка, супи й поради, що зроблять тебе кулінаром.
+`,
+    });
+    this.meta.updateTag({ name: 'imageUrl', content: this.mainImage });
+    this.meta.updateTag({ name: 'author', content: 'Yurii Ohlii' });
+    this.meta.updateTag({
+      property: 'fb:app_id',
+      content: '433617998637385',
+    });
+    this.meta.updateTag({
+      property: 'og:title',
+      content: `Таверна Синій Кіт — кулінарні рецепти для гурманів: м'ясо, риба, салати, випічка, десерти`,
+    });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: `Таверна 'Синій Кіт' — ваш путівник у світі кулінарії! Широкий вибір рецептів: м’ясо, риба, овочі, пасти, салати, супи, випічка, десерти. Ідеї для кожного гостя і кожного столу!`,
+    });
+    this.meta.updateTag({
+      property: 'keywords',
+      content: `рецепти, таверна Синій Кіт, кулінарні рецепти, страви, м'ясо, риба, овочі, пасти, салати, супи, соуси, десерти, випічка, домашня кухня, кулінарні поради, гастрономія`,
+    });
+    this.meta.updateTag({ property: 'og:image', content: this.mainImage });
+    this.meta.updateTag({ property: 'og:url', content: this.currentURL });
+
+    this.schema = {
+      '@context': 'https://schema.org',
+      '@type': 'WebSite',
+      name: this.homeTite,
+      url: 'https://tsk.in.ua',
+      description: `У нашій колекції рецептів ви знайдете страви на будь-який смак і випадок: соковите м’ясо, ніжну рибу, легкі овочеві страви, різноманітні соуси, ароматні супи, домашню випічку, десерти й багато іншого. Усе продумано, щоб зробити процес приготування максимально простим і приємним.
+Перегляньте наші основні категорії й оберіть те, що підійде для вашого столу. Таверна "Синій Кіт" пропонує як класичні рецепти, так і сучасні ідеї для кулінарних експериментів.
+`,
+      image: this.mainImage,
+      publisher: {
+        '@type': 'Person',
+        name: 'Оглій Юрій',
+        url: 'https://tsk.in.ua',
+      },
+      potentialAction: {
+        '@type': 'SearchAction',
+        target: 'https://tsk.in.ua/recipe-filter?q={search_term_string}',
+
+        'query-input': 'required name=search_term_string',
+      },
+    };
+
+    this.setSchema(this.schema);
+  }
+
+  setSchema(schema: any): void {
+    if (this.ldJsonScript) {
+      this.renderer.removeChild(this.document.head, this.ldJsonScript);
+    }
+    const script = this.renderer.createElement('script');
+    script.type = 'application/ld+json';
+    script.text = JSON.stringify(schema);
+    this.renderer.appendChild(this.document.head, script);
+    this.ldJsonScript = script;
+  }
+
+
+  async recipeCountLoad() {
+    for (let dish of this.dishesList) {
+      this.recipeCounts[dish.id] = await this.recipeService.getRecipeCount(dish.id);
+
+      this.categoryService.getLightById(dish.id).subscribe(categories => {
+        this.dishCategories[dish.id] = categories;
+        this.dishCategories[dish.id].sort((a, b) => a.categoryName.localeCompare(b.categoryName));
+      });
+    }
+  }
+
+  // Відстежування події прокрутки вікна
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    if (!this.isBrowser) return;
+    const scrollPosition = window.scrollY;
+
+    //анімація опису
+    const elementPosition =
+      this.textBlocksRef?.nativeElement.getBoundingClientRect().top +
+      window.scrollY;
+    const elementHeight = this.textBlocksRef?.nativeElement.offsetHeight;
+    if (
+      scrollPosition + window.innerHeight >
+      elementPosition + elementHeight / 6
+    ) {
+      this.isVisible = true;
+    }
+
+    //анімація карток
+    const dishesBlock = document.querySelectorAll('.dishes_block');
+    dishesBlock.forEach((card: Element) => {
+      const htmlCard = card as HTMLElement;
+      const elementPosition =
+        htmlCard.getBoundingClientRect().top + window.scrollY;
+
+      const elementHeight = htmlCard.offsetHeight; // Висота елемента
+      // Перевірка, чи елемент потрапляє в видиму область (екран)
+      if (
+        scrollPosition + window.innerHeight >
+        elementPosition + elementHeight / 2
+      ) {
+        // Додаємо клас для активації анімації чи зміни стилю
+        htmlCard.classList.add('show');
+      }
+    });
+  }
+
+
+
+
+}
