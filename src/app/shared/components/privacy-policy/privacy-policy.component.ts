9631a589bd38a1a121e6f3f61fa28fa0189ca4ec
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2025-07-27 11:57:14 +0300

    privacy-policy

--- src/app/shared/components/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/components/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+
+  cookieAccepted = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.cookieAccepted = localStorage.getItem('cookieConsent') === 'true';
+    }
+  }
+
+  acceptCookies(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('cookieConsent', 'true');
+    }
+    this.cookieAccepted = true;
+  }
 
 }
