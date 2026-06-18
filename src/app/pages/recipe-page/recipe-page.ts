87e2363b18cbb1c68df3b4195f88f9c30a7d5970
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2026-06-18 10:18:40 +0300

    Виправлено прокрутку меню рецепта, блок схожих рецептів перенесено після FAQ

--- src/app/pages/recipe-page/recipe-page.ts
+++ src/app/pages/recipe-page/recipe-page.ts
@@ -309,7 +309,7 @@ export class RecipePage {
     const el = this.document.getElementById(item.id);
 
     if (el) {
-      const y = el.getBoundingClientRect().top + window.scrollY - this.totalOffset();
+    const y = el.offsetTop - this.totalOffset();
 
       window.scrollTo({
         top: y,
